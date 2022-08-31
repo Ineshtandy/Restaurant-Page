234f1832224f382ca71b05c4ab7da464d6e4b09a
Inesh <i08polymath@gmail.com>
2022-08-31 05:58:50 +0530

    SRC JS FILE UPDATE
    dynamically creating html content and loading on the page
    to see every change run 'npx webpack' command on terminal and then open
    dist html file
    
    added pizza_img to the root directory

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 console.log("seems like npx ran");
+
+const container = document.querySelector('#content');
+
+let headerelm = document.createElement('h1');
+headerelm.textContent = "Tandy's";
+
+let imgelm = document.createElement('img');
+imgelm.src = "../pizza_img.jpg";
+
+let second_header_elm = document.createElement('h3');
+second_header_elm.textContent = "Hungry? Delighful,let us server you!";
+
+let description_elm = document.createElement('div');
+description_elm.textContent = "Come home to a mouth watering selection of authentic italian delicacies";
+
+let reviews_elm = document.createElement('div');
+reviews_elm.textContent = "REVIEWS";
+
+let footer_elm = document.createElement('footer');
+footer_elm.textContent = "Thank You";
+
+container.appendChild(headerelm);
+container.appendChild(imgelm);
+container.appendChild(second_header_elm);
+container.appendChild(description_elm);
+container.appendChild(reviews_elm);
+container.appendChild(footer_elm);
